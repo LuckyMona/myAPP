b087716964431851c08d5e26d4cff1a65ed79662
SuperMary <miao.hnlk@foxmail.com>
2016-06-21 18:24:10 +0800

    upload

--- www/js/app.js
+++ www/js/app.js
@@ -108,7 +108,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/taskList');
+  $urlRouterProvider.otherwise('/tab/uploads');
   //$urlRouterProvider.otherwise('/login/active');
 
 });
